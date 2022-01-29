547d9b84218e2a9f975d59a13d1611f242a23ed1
Kakit Ho <hkk565@gmail.com>
2022-01-29 09:52:32 -0800

    Update jpg_converter.sh

--- jpg_converter.sh
+++ jpg_converter.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 src_dir=$1
-des_dir=$2
 
-mkdir -p $des_dir
+if [ $# == 1 ]
+then
+    des_dir=$1
+else
+    des_dir=$2
+fi
 
-sips -s format jpeg -s formatOptions high ${src_dir%%/}/*.{bmp,BMP,heic,HEIC,png,PNG} --out $des_dir
+mkdir -p ${des_dir}
+
+sips -s format jpeg -s formatOptions high ${src_dir%%/}/*.{bmp,BMP,heic,HEIC,png,PNG} --out ${des_dir}
+rm ${des_dir%%/}/*.{bmp,BMP,heic,HEIC,png,PNG}
